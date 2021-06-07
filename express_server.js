5df026f5047d58ae04ee0c82ec94710a49e7c8a1
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-07 15:34:12 +0000

    added new routes to server for the new views added

--- express_server.js
+++ express_server.js
@@ -2,24 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-//set the view engine to ejs
-app.set('view engine', 'ejs');
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+//set the view engine to ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-})
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
