0f84bd48f40672186d2fe057a8c7dae7424ccd31
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-07 21:19:35 +0000

    made changes to express_server.js to incorporate the urls_new.ejs view

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080;
 
 const urlDatabase = {
@@ -7,20 +8,41 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const generateRandomString = function(){
+  let result = '';
+  result = Math.random().toString(36).substr(2,6); 
+  return result;
+}
+
+generateRandomString();
+
 //set the view engine to ejs
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+//middlewear
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req,res)=> {
+  console.log(req.body);
+  res.send("ok");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
 
