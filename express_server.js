f18eee28317c6543750bf40d0a2c9d0d1a3b659a
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-09 13:17:39 +0000

    completed edge cases to avoid duplicate registration or incomplete registration (ie.blank password field)

--- express_server.js
+++ express_server.js
@@ -7,33 +7,27 @@ const { json } = require("body-parser");
 const PORT = 8080;
 
 //delcare urlDatabase as a global variable so entire application can access values
-const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-};
+const urlDatabase = {};
 
 //store an create new users using object
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+const users = {};
 
 //set username to global variable so it is available throughout application
-let = username = "";
+let username = "";
 
 //generate random alphanumeric string of length 6. This is used when creating a new shortURL
 const generateRandomString = function () {
   let result = "";
   result = Math.random().toString(36).substr(2, 6);
   return result;
+};
+
+//check registered users email to avoid duplicate accounts
+const checkUserEmail = function (email, userObject) {
+  for (user in userObject) {
+    if (email === user.email) return false;
+    return true;
+  }
 };
 
 //set the view engine to ejs
@@ -43,21 +37,29 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// get requests
+// get urls page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, users: users };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
   res.render("urls_index", templateVars);
 });
 
-//get new url page
+//get new urls page
 app.get("/urls/new", (req, res) => {
-  const templateVars = { users: users };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 //get register page
 app.get("/register", (req, res) => {
-  res.render("urls_register");
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user_id"]],
+  };
+  console.log(users);
+  res.render("urls_register", templateVars);
 });
 
 //get shortURL application page
@@ -65,7 +67,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    users: users,
+    user: users[req.cookies["user_id"]],
   };
   res.render("urls_show", templateVars);
 });
@@ -78,16 +80,25 @@ app.get("/u/:shortURL", (req, res) => {
 
 //register new user
 app.post("/register", (req, res) => {
-  const id = generateRandomString();
-  users[id] = {
-    id: id,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  console.log(users[id])
-  console.log(users)
-  res.cookie("user_id", users[id]);
-  res.redirect("/urls");
+  if (req.body.email === "" || req.body.password === "") {
+    res.status(400).send("Please fill out all required fields");
+  }
+  if (checkUserEmail(req.body.email, users) === true) {
+    res
+      .status(400)
+      .send("Email already registered, please login or use a different email");
+  }
+
+  if (res.statusCode !== 400) {
+    const userId = generateRandomString();
+    users[userId] = {
+      id: userId,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    res.cookie("user_id", users[userId]["id"]);
+    res.redirect("/urls");
+  }
 });
 
 //login to app
